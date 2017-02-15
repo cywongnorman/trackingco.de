b07864668f30bcac265a6de09aa76fcae662bfdd
fiatjaf <fiatjaf@gmail.com>
2017-02-15 15:58:27 -0500

    a basic chart with recharts.

--- client/app.js
+++ client/app.js
@@ -3,8 +3,9 @@ const render = require('react-dom').render
 const h = require('react-hyperscript')
 const Lokka = require('lokka').Lokka
 const Transport = require('lokka-transport-http').Transport
+const R = require('recharts')
 
-const graphql = new Lokka({ transport: new Transport('/_graphql') })
+const graphql = new Lokka({transport: new Transport('/_graphql')})
 
 const Card = React.createClass({
   getInitialState () {
@@ -18,7 +19,8 @@ const Card = React.createClass({
       site(code: $code) {
         name
         code
-        days(last:14) {
+        days(last:7) {
+          day
           sessions
           pageviews
         }
@@ -47,13 +49,26 @@ const Card = React.createClass({
         h('h6.card-meta', this.state.site.code)
       ]),
       h('.card-image', [
-
+        h(R.ResponsiveContainer, {height: 200, width: '100%'}, [
+          h(R.ComposedChart, {data: this.state.site.days}, [
+            h(R.Bar, {
+              dataKey: 'sessions',
+              fill: '#8884d8',
+              isAnimationActive: false
+            }),
+            h(R.Line, {
+              dataKey: 'pageviews',
+              stroke: '#82ca9d',
+              isAnimationActive: false
+            })
+          ])
+        ])
       ])
     ])
   }
 })
 
 render(
-  React.createElement(Card, { code: 'ciz4n47q800010z090rwz1rxn' }),
+  React.createElement(Card, {code: 'ciz4n47q800010z090rwz1rxn'}),
   document.getElementById('main')
 )
