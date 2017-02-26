768c18c52630a2b510de63047e0971e4f13ca082
fiatjaf <fiatjaf@gmail.com>
2017-02-25 20:55:37 -0500

    make cronjob run at 00:01 UTC.

--- routines.go
+++ routines.go
@@ -9,7 +9,7 @@ import (
 func daily() {
 	var day string
 	pflag.StringVar(&day, "day",
-		presentDay().AddDate(0, 0, -1).Format(DATEFORMAT),
+		presentDay().Format(DATEFORMAT),
 		"compile stats from which day?")
 	pflag.Parse()
 
